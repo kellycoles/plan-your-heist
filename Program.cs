767d629de618fc2172a7ac4471eded15d84ca2e5
kellycoles <kellycoles1@gmail.com>
2019-10-08 18:39:46 -0500

    added classes

--- Program.cs
+++ Program.cs
@@ -1,60 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace PlanYourHeist {
-    class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Plan your heist!");
+namespace PlanYourHeist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan Your Heist!");
 
-            List<Dictionary<string, string>> team = new List<Dictionary<string, string>> ();
-            Console.Write ("Name> ");
-          
-            
-            string name = Console.ReadLine ();
-            Console.WriteLine ();
-            while (name != "") {
-                Console.Write ("skill Level> ");
-                string skillLevel = Console.ReadLine ();
+            List<TeamMember> team = new List<TeamMember>();
 
-                Console.Write ("courage Factor> ");
-                string courageFactor = Console.ReadLine ();
-                Dictionary<string, string> member = new Dictionary<string, string> () { { "Name", name }, { "SkillLevel", skillLevel }, { "Courage Factor", courageFactor }
-                };
-                team.Add (member);
-                Console.WriteLine ();
-                Console.Write ("Name> ");
-                name = Console.ReadLine ();
+            Console.Write("Bank Difficulty> ");
+            int bankDifficulty = int.Parse(Console.ReadLine());
+            Console.WriteLine();
 
+            // Get first team member's name
+            Console.Write("Name> ");
+            string name = Console.ReadLine();
+
+            while (name != "")
+            {
+                Console.Write("Skill level> ");
+                string skillLevel = Console.ReadLine();
+
+                Console.Write("Courage factor> ");
+                string courageFactor = Console.ReadLine();
+
+                TeamMember member = new TeamMember();
+                member.Name = name;
+                member.SkillLevel = int.Parse(skillLevel);
+                member.CourageFactor = double.Parse(courageFactor);
+
+                team.Add(member);
+
+                Console.WriteLine();
+
+                // Get next team member's name
+                Console.Write("Name> ");
+                name = Console.ReadLine();
             }
-            Console.Write("Please, enter number of runs> ");
-            int trialRunCount =  int.Parse(Console.ReadLine());
-           
-            Console.WriteLine (
-                $"Team Size:{team.Count}"
-            );
 
+            Console.WriteLine($"Team Size: {team.Count}");
+
+            Console.WriteLine();
+            Console.WriteLine("Enter number of trial runs>");
+            int trialRunCount = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            // Determine total skill level of the team and compare it to the bank's difficulty level
             int teamSkill = 0;
-            foreach (Dictionary<string, string> member in team) {
-                string skillLevel = member["SkillLevel"];
-                teamSkill = teamSkill + int.Parse(skillLevel);
+
+            foreach (TeamMember member in team)
+            {
+                teamSkill += member.SkillLevel;
             }
-           for (int i = 0; i < trialRunCount; i++) //we wrapped code below in for loop. every time through the loop i'll get a new luckvalue and bank difficulty
-           {
-               Random generator = new Random(); //phase 4  gets random number
-               int luckValue = generator.Next(-10, 11); //takes minval and maxvalue
-               int bankDifficulty = 100; //needed to bring this down from the top as well.
-               bankDifficulty += luckValue; //add luckvalue to bankdifficulty and set the value
-               Console.WriteLine($"Team Skil level: {teamSkill}");
-               Console.WriteLine($"Bank Difficulty: {bankDifficulty}");
-               if (bankDifficulty > teamSkill) //did the team do better than the bank difficulty level?
-               {
-                   Console.WriteLine("Your heist failed :(");
-               }
-               else
-               {
-                   Console.WriteLine("You are rich!"); //run it and see if you failed or not
-               }
-           }
+
+            HeistReport report = new HeistReport();
+
+            for (int i = 0; i < trialRunCount; i++)
+            {
+                // Create a random number between -10 and 10 for the heist's luck value. Add this number to the bank's difficulty level.
+                Random generator = new Random();
+                int luckValue = generator.Next(-10, 11);
+                int trialRunBankDifficulty = bankDifficulty + luckValue;
+
+                Console.WriteLine($"Team Skill Level: {teamSkill}");
+                Console.WriteLine($"Bank Difficulty Level: {trialRunBankDifficulty}");
+
+                if (trialRunBankDifficulty > teamSkill)
+                {
+                    report.FailureCount++;
+                }
+                else
+                {
+                    report.SuccessCount++;
+                }
+
+                Console.WriteLine("---------------------------");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Heist Results:");
+            Console.WriteLine($"Successes: {report.SuccessCount}");
+            Console.WriteLine($"Failures: {report.FailureCount}");
+            Console.WriteLine("---------------------------");
         }
     }
 }
---
+++ teamMember.cs
@@ -0,0 +1,8 @@
+namespace PlanYourHeist {
+    public class TeamMember {
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public double CourageFactor { get; set; }
+        
+    }
+}
